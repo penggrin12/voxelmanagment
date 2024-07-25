c61ca5e780e562baeeab59e6f3900f1254ac3ad2
penggrin12 <miner.sidor@gmail.com>
2024-07-25 14:59:42 +0300

    `Location.ToString` & `Location.GetGlobalPosition`

--- src/Structs.cs
+++ src/Structs.cs
@@ -12,4 +12,7 @@ public struct Location
 {
     public Vector2I chunkPosition;
     public Vector3I voxelPosition;
+
+    public override readonly string ToString() { return $"[{voxelPosition} in {chunkPosition}]"; }
+    public readonly Vector3I GetGlobalPosition() { return new(voxelPosition.X + (chunkPosition.X * Chunk.CHUNK_SIZE.X), voxelPosition.Y, voxelPosition.Z + (chunkPosition.Y * Chunk.CHUNK_SIZE.X)); }
 }
